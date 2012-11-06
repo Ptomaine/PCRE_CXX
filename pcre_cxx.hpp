54dcb2726dbfe8a3447e06e479d3466f4a9f7f93
Arlen Keshabyan <arlen.albert@gmail.com>
2012-11-06 15:29:10 +0400

    Update pcre_cxx.hpp

--- pcre_cxx.hpp
+++ pcre_cxx.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 /***************************************************************************
- *   Copyright (C) 2008, 2012 by Arlen Albert Keshabyan                    *
+ *   Copyright (C) 2008, 2012 by Arlen Keshabyan                           *
  *   <arlen.albert@gmail.com>                                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
